7e60a0b3b752e40a8ed3f25ed0794ffd3f896e44
JustinRyan <justinr922@gmail.com>
2020-08-15 00:30:12 -0400

    added Canvas Generator Function

---
+++ canvasController.js
@@ -0,0 +1,13 @@
+function generateCanvas(n) {
+    let canvas = document.querySelector('#sketchBoard');
+    for (let row = 0; row < n; row++) {
+        let row = document.createElement('div');
+        row.classList.add("canvasRow");
+        canvas.appendChild(row);
+        for (let column = 0; column < n; column++) {
+            let cell = document.createElement('div');
+            cell.classList.add('canvasColumn');
+            row.appendChild(cell);
+        }
+    }
+}
