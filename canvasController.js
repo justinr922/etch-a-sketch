6513147fc5a3ac5625a530c0f75659e6fc92885a
JustinRyan <justinr922@gmail.com>
2020-08-15 02:16:53 -0400

    hover functionality and flexbox fix

--- canvasController.js
+++ canvasController.js
@@ -6,24 +6,32 @@ document.querySelector('#heightWidthSubmit').addEventListener('click',
     }
 )
 
+function createHover(object) {
+    object.addEventListener('mouseenter', function() {
+        object.style.backgroundColor = '#000000'
+    })
+}
+
 function generateCanvas(n) {
     let canvas = document.querySelector('#sketchBoard');
     for (let row = 0; row < n; row++) {
         let row = document.createElement('div');
-        row.classList.add("cell");
+        row.classList.add('row');
         canvas.appendChild(row);
         for (let column = 0; column < n; column++) {
             let cell = document.createElement('div');
             cell.classList.add('cell');
             row.appendChild(cell);
+            createHover(cell);
         }
-        console.log('Adding Row: ', row+1);
     }
 }
 
 function clearCanvas() {
     let canvas = document.querySelector('#sketchBoard');
     while (canvas.firstChild) {
+        canvas.lastChild = null;
         canvas.removeChild(canvas.lastChild);
+
     }
 }
